68a8844da1aed5a0d375c89eeb8dbb3ac869e01f
carlromeo <carlromeo.dogbey@gmail.com>
2019-10-22 11:44:59 -0400

    Read me

---
+++ src/main/java/com/example/demo/WinterSaharaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterSaharaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinterSaharaApplication.class, args);
+    }
+
+}
